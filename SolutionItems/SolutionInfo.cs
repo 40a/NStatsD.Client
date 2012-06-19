d256b40994b9c8fd0f51a77fedcf9d1dbc97d4a5
Neutmute <wintermute111@gmail.com>
2012-06-19 22:25:49 +1000

    better assembly metadata

---
+++ SolutionItems/SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Rob Bihun")]
+[assembly: AssemblyCopyright("Copyright © Rob Bihun 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: System.CLSCompliant(false)]
+[assembly: ComVisible(false)]
+
+
